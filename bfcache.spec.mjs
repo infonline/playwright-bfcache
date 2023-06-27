1337bf7d626b7d0fd690f0399f36c2bd98adf5fb
Matthias Böhm <mboehm@infonline.de>
2023-06-27 13:44:16 +0200

    Initial commit

---
+++ bfcache.spec.mjs
@@ -0,0 +1,32 @@
+import { chromium, expect, firefox, test } from '@playwright/test';
+
+const browsers = [
+    {
+        instance: chromium,
+        name: 'Chromium',
+        options: {
+            headless: false,
+        },
+    },
+    {
+        instance: firefox,
+        name: 'Firefox',
+        options: {},
+    },
+];
+
+browsers.map(async (browser) => {
+    test(`[${browser.name}] bfcache demo`, async () => {
+        const browserInstance = await browser.instance.launch(
+            browser.options,
+        );
+        const page = await browserInstance.newPage();
+
+        const [iombRequest, iompRequest] = await Promise.all([
+            page.goto(
+                `http://localhost:8080/page1`,
+            ),
+            page.waitForTimeout(6000000),
+        ]);
+    })
+});
